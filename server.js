4fbbde01cad4496e84bbe593abcfe35e9dff908b
Aditya <adityanehare571@gmail.com>
2025-04-27 16:03:13 +0530

    Initial setup ejs, ejs-mate create views folder in that i added boilerplate, courses, home, partial

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const engine = require("ejs-mate");
 
 const app = express();
 const port = process.env.PORT;
@@ -11,8 +12,17 @@ mongoose
   .then(() => console.log("Connected to MongoDB Database"))
   .catch((err) => console.log(err, " Error in connecting the DataBase"));
 
-app.get("/HTML", (req, res) => {
-  res.send("Hello From HTML");
+app.engine("ejs", engine);
+app.set("views", __dirname + "/views");
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home/home", { title: "SkillCraft - Learn and Build!" });
+});
+
+app.get("/html", (req, res) => {
+  res.render("courses/html");
 });
 
 app.listen(port, () => {
